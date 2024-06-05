a8d09258541380c44e165092b176c252a1de4ebd
Subhrajeet Sikdar <108749251+SubhrajeetSikdar@users.noreply.github.com>
2024-06-06 00:12:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,6 @@ for entry in requestList:
 		processInit(pid)
 	requests[pid].append((rw, vaddr))
 thread_mmu = threading.Thread(target=mmu)
-thread_os_scheduler = threading.Thread(target=scheduler)
+thread_os_scheduler=threading.Thread(target=scheduler)
 thread_os_scheduler.start();
 thread_mmu.start();
