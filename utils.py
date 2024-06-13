fbc706220a64de88dc4fa3586b0ba967e02cdd70
Subhrajeet Sikdar <108749251+SubhrajeetSikdar@users.noreply.github.com>
2024-06-13 20:28:31 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,16 +2,16 @@ def inttohex(int_):
     if int_ >= 0:
         return ("{0:0>4s}".format(hex(int_ % (1 << 16))[2:])).upper()
     else:
-        return (hex((int_ + (1 << 16)) % (1 << 16)).upper()[2:]).upper()
+        return (hex((int_ + (1 << 16))%(1 << 16)).upper()[2:]).upper()
 
 def inttobin(int_):
     if int_ >= 0:
         return "{0:0>16s}".format(bin(int_)[2:])
     else:
-        return bin((int_ + (1 << 16)) % (1 << 16)).upper()[2:]
+        return bin((int_ +(1 << 16))%(1 << 16)).upper()[2:]
     
 def hextobin(hex_):
-    return inttobin(int(hex_, 16))
+    return inttobin(int(hex_,16))
 
 def bintohex(bin_):
-    return inttohex(int(bin_, 2))
+    return inttohex(int(bin_,2))
